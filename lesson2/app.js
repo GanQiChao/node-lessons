125f83a1911a523e67d59d92283f21653e71c15d
chao <gqcup@qq.com>
2019-09-07 13:14:23 +0800

    学习 lesson1~3

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,14 +1,24 @@
-var express = require('express');
-var utility = require('utility');
-var app = express();
+const express = require('express')
+const utility = require('utility')
+const app = express()
 
-app.get('/', function (req, res) {
-  var q = req.query.q;
-  var md5Value = utility.md5(q);
+app.get('/', (req, res) => {
+  // change query param to md5/sha1
+  console.log(req.query)
+  const { md5, sha1 } = req.query
+  if (md5) {
+    var md5Value = utility.md5(md5)
 
-  res.send(md5Value);
-});
+    res.send(`md5 = ${md5Value}`)
+  } else if (sha1) {
+    var sha1Value = utility.sha1(sha1)
 
-app.listen(3000, function (req, res) {
-  console.log('app is running at port 3000');
-});
+    res.send(`sha1 = ${sha1Value}`)
+  } else {
+    res.send('not allow param, please use md5 or sha1.')
+  }
+})
+
+app.listen(3000, (req, res) => {
+  console.log('app is running at port 3000')
+})
--- lesson3/app.js
+++ lesson3/app.js
@@ -1,30 +1,35 @@
-var express = require('express');
-var cheerio = require('cheerio');
-var superagent = require('superagent');
+const express = require('express')
+// 分析网页：Node.js 下的 jQuery
+const cheerio = require('cheerio')
+// 抓取网页：http 库，用于发起 get 或 post
+const superagent = require('superagent')
 
-var app = express();
+const app = express()
 
-app.get('/', function (req, res, next) {
-  superagent.get('https://cnodejs.org/')
-    .end(function (err, sres) {
-      if (err) {
-        return next(err);
-      }
-      var $ = cheerio.load(sres.text);
-      var items = [];
-      $('#topic_list .topic_title').each(function (idx, element) {
-        var $element = $(element);
-        items.push({
-          title: $element.attr('title'),
-          href: $element.attr('href')
-        });
-      });
+app.get('/', (req, res, next) => {
+  // 抓取网页数据
+  superagent.get('https://cnodejs.org/').end(function(err, sres) {
+    if (err) {
+      return next(err)
+    }
+    // sres.text 是抓取页面的返回内容 html
+    // $ 命名，因类似 jQuery 功能
+    const $ = cheerio.load(sres.text)
+    const items = []
+    $('#topic_list .cell').each(function(idx, element) {
+      const $author = $(element).find('img')
+      const $title = $(element).find('.topic_title')
+      items.push({
+        author: $author.attr('title'),
+        title: $title.attr('title'),
+        href: $title.attr('href')
+      })
+    })
 
-      res.send(items);
-    });
-});
+    res.send(items)
+  })
+})
 
-
-app.listen(3000, function () {
-  console.log('app is listening at port 3000');
-});
+app.listen(3000, function() {
+  console.log('app is listening at port 3000')
+})
