8ba2fbfbc2d51ecd1cd5874fb0c19fdfbb0a2cec
chao <gqcup@qq.com>
2019-09-10 00:08:44 +0800

    完成lesson4，并发40个请求经常不能完成后面2、3个，原因估计是服务器拦截并发，也能是异步问题

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,47 +1,71 @@
-var eventproxy = require('eventproxy');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
-var url = require('url');
+const eventproxy = require('eventproxy')
+const superagent = require('superagent')
+const cheerio = require('cheerio')
+const url = require('url')
+// const express = require('express')
+// const app = express()
 
-var cnodeUrl = 'https://cnodejs.org/';
+const cnodeUrl = 'https://cnodejs.org/'
 
-superagent.get(cnodeUrl)
-  .end(function (err, res) {
-    if (err) {
-      return console.error(err);
-    }
-    var topicUrls = [];
-    var $ = cheerio.load(res.text);
-    $('#topic_list .topic_title').each(function (idx, element) {
-      var $element = $(element);
-      var href = url.resolve(cnodeUrl, $element.attr('href'));
-      topicUrls.push(href);
-    });
+// app
+//   .get('/', (req, response, next) => {
+superagent.get(cnodeUrl).end((err, res) => {
+  // if (err) {
+  //   return next(err)
+  // }
+  const topicUrls = []
+  const $ = cheerio.load(res.text)
+  $('#topic_list .topic_title').each((idx, element) => {
+    const $element = $(element)
+    const href = url.resolve(cnodeUrl, $element.attr('href'))
+    topicUrls.push(href)
+  })
+  topicUrls.splice(5, 40)
+  // response.send(topicUrls)
 
-    var ep = new eventproxy();
+  const ep = new eventproxy()
 
-    ep.after('topic_html', topicUrls.length, function (topics) {
-      topics = topics.map(function (topicPair) {
-        var topicUrl = topicPair[0];
-        var topicHtml = topicPair[1];
-        var $ = cheerio.load(topicHtml);
-        return ({
-          title: $('.topic_full_title').text().trim(),
-          href: topicUrl,
-          comment1: $('.reply_content').eq(0).text().trim(),
-        });
-      });
+  ep.fail(err => console.log(err))
 
-      console.log('final:');
-      console.log(topics);
-    });
+  ep.after('topic_html', topicUrls.length, topics => {
+    topics = topics.map(function(topicPair) {
+      const topicUrl = topicPair[0]
+      const topicHtml = topicPair[1]
+      const $ = cheerio.load(topicHtml)
+      const reply = $('.reply_item').eq(0)
+      return {
+        title: $('.topic_full_title')
+          .text()
+          .trim(),
+        href: topicUrl,
+        comment1: reply
+          .find('.reply_content')
+          .text()
+          .trim(),
+        author: reply.find('.author_content img').attr('title'),
+        score: reply
+          .find('.user_action .up-count')
+          .text()
+          .trim()
+      }
+    })
 
-    topicUrls.forEach(function (topicUrl) {
-      superagent.get(topicUrl)
-        .end(function (err, res) {
-          console.log('fetch ' + topicUrl + ' successful');
-          ep.emit('topic_html', [topicUrl, res.text]);
-        });
-    });
-  });
+    console.log('final:')
+    console.log(topics)
 
+    // response.send(topics)
+  })
+
+  let i = 1
+  topicUrls.forEach(topicUrl => {
+    superagent.get(topicUrl).end((err, res) => {
+      // if (err) next(err)
+      console.log(`${i++} - fetch ${topicUrl} successful`)
+      ep.emit('topic_html', [topicUrl, res.text])
+    })
+  })
+})
+// })
+// .listen(3000, () => {
+//   console.log('app is listening at port 3000')
+// })
